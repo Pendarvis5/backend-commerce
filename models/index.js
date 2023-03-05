e07b6ff268acc76c291bd902668654ac18bb80aa
Nicole Marie <nicolependarvis5@gmail.com>
2023-03-05 16:14:19 -0500

    api updates

--- models/index.js
+++ models/index.js
@@ -1,16 +1,20 @@
-// import models
+
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.Category = Product.belongsTo(Category);
+
+Category.Product = Category.hasMany(Product);
+
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.ProductTag = Product.belongsToMany(ProductTag);
 // Tags belongToMany Products (through ProductTag)
+const ProductTag = Tag.belongsToMany(Product,{ as: 'ProductTag'});
+
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,22 +3,38 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/Category', (req, res) => {
   // find all categories
+  Category.findAll({
+
+  })
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findByPk(req.params.id).then((categoryData) => {
+      res.json(categoryData);
+  })
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    
+  })
 });
 
 router.delete('/:id', (req, res) => {
